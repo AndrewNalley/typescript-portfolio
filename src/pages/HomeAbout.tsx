e758a1d991e744ffe5642cf1d5784c89d3eaf215
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-11-15 14:09:11 -0600

    update headshot

--- src/pages/HomeAbout.tsx
+++ src/pages/HomeAbout.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import headshot from '../images/headshot.jpg';
+import headshot from '../images/headshot_2023_focus.jpeg';
 
 function HomeAbout() {
     return (
         <section className="container" id="about-me">
             <h4 className="heading-center">About Me</h4>
             <article className="grid">
-                <img className="headshot" src={headshot} alt='headshot' />
+                <div id="img-div">
+                    <img className="headshot" src={headshot} alt='headshot' />
+                    <h4 className="heading-center">Andrew Nalley</h4>
+                </div>
                 <div>
                     <blockquote>I am a Full Stack Web Developer with a certificate from the University of Minnesota
-                        Web Development Bootcamp, with previous experience as an Administrator and Opera Singer.<br/><br/>
+                        Web Development Bootcamp, with previous experience as an Administrator and Opera Singer.<br /><br />
                         As professional with a flair for the aesthetic side of things, I have been
-                        drawn to the many facets of development and design. <br/><br/>
+                        drawn to the many facets of development and design. <br /><br />
                         I can't wait to bring my skills and years of professional experience to your
                         project!
                     </blockquote>
