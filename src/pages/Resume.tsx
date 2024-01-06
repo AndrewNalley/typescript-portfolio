757bc5115bb22e6aefd6dbe6b047f9c058eede9b
Andrew Nalley <awnalley@gmail.com>
2024-01-06 14:23:36 -0600

    add loading notification when resume is being retrieved

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Resume() {
+    const [loading, setLoading] = useState(true);
+
     // pdf from google drive - editable google doc undesireably affected by responsive design
     // search 'share google pdf on webpage' for update help
     // upload pdf to drive, share (accessible to all), then copy and paste the portion noted within both links below 
@@ -10,14 +12,21 @@ function Resume() {
     const currentResPDF = 'https://drive.google.com/file/d/1wcovGHD7_wyXvWv1GG-inH0kffncupCf/preview';
     const pdfDownloadLink = 'https://drive.google.com/uc?export=download&id=1wcovGHD7_wyXvWv1GG-inH0kffncupCf';
 
+    const handleLoad = () => {
+        // This function will be called when the iframe has finished loading
+        setLoading(false);
+    };
+
     return (
         <>
             <h4 className="heading-center">Resume</h4>
+            {loading && <p>Loading...</p>}
             <div className='iframe-container'>
                 <iframe
                     className='responsive-iframe'
                     title='Google Doc'
                     src={currentResPDF}
+                    onLoad={handleLoad}
                 ></iframe>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
